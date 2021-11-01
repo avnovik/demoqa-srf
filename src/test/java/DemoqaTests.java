deca57ba2560e43de7f275056379e6d70eee99c1
Novikov Aleksei <3900676@gmail.com>
2021-11-01 17:12:54 +0300

    work accepted, final edits made

--- src/test/java/DemoqaTests.java
+++ src/test/java/DemoqaTests.java
@@ -1,11 +1,9 @@
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class DemoqaTests {
     @Test
@@ -13,12 +11,12 @@ public class DemoqaTests {
         //Configuration.startMaximized = true;
         String link = "https://demoqa.com/automation-practice-form",
                 welcomeText = "Student Registration Form",
-                fName = "Frodo",
-                sName = "Bagins",
+                firstName = "Frodo",
+                lastName = "Bagins",
                 email = "test@test.test",
-                telNum = "0123456789",
-                subInput1 = "b",
-                subInput2 = "ar",
+                telephoneNumber = "0123456789",
+                subjectInput1 = "b",
+                subjectInput2 = "ar",
                 uploadFileName = "test.pic.txt",
                 address = "Yorkwir 3ii Bugorok - nora №2",
                 state = "d",
@@ -26,18 +24,18 @@ public class DemoqaTests {
 
         open(link);
         $(".practice-form-wrapper").shouldHave(text(welcomeText));
-        $("#firstName").setValue(fName);
-        $("#lastName").setValue(sName);
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
-        $("[for=\"gender-radio-3\"]").click();
-        $("#userNumber").val(telNum);
+        $("[for='gender-radio-3']").click();
+        $("#userNumber").val(telephoneNumber);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").$("[value=\"5\"]").click();
-        $(".react-datepicker__year-select").$("[value=\"1955\"]").click();
+        $(".react-datepicker__month-select").$("[value='5']").click();
+        $(".react-datepicker__year-select").$("[value='1955']").click();
         $(".react-datepicker__day--001").click();
-        $("#subjectsInput").setValue(subInput1).pressTab();
-        $("#subjectsInput").setValue(subInput2).pressTab();
-        $("[for=\"hobbies-checkbox-3\"]").click();
+        $("#subjectsInput").setValue(subjectInput1).pressTab();
+        $("#subjectsInput").setValue(subjectInput2).pressTab();
+        $("[for='hobbies-checkbox-3']").click();
         $("#uploadPicture").uploadFromClasspath(uploadFileName);
         $("#currentAddress").setValue(address);
         $("#react-select-3-input").setValue(state).pressTab();
@@ -46,8 +44,14 @@ public class DemoqaTests {
         $("#submit").click();
         //verification
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".modal-content .modal-body").shouldHave(text(fName), text(sName), text(email), text(telNum),
-                text(uploadFileName), text(address), text(city));
+        $(".modal-content .modal-body").shouldHave(
+                text(firstName),
+                text(lastName),
+                text(email),
+                text(telephoneNumber),
+                text(uploadFileName), 
+                text(address),
+                text(city));
         $("#closeLargeModal").scrollIntoView(true).click();
     }
 }
