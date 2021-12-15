e2a8345d2fe08b1fd830287ec6d619b18fe979fe
Novikov Aleksei <3900676@gmail.com>
2021-12-15 14:50:30 +0300

    add remote config

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,14 +1,25 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 import pages.VerificationPage;
 
 public class TestBase {
 
-    RegistrationPage registrationPage = new RegistrationPage();
-    VerificationPage verificationPage = new VerificationPage();
 
     @BeforeAll
     static void beforeAll() {
         //Configuration.browserSize = "800x600";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
     }
+
+
+    RegistrationPage registrationPage = new RegistrationPage();
+    VerificationPage verificationPage = new VerificationPage();
 }
